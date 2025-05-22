6c2b1854423b318595aea34b53344d28255cc99c
Shaun Wilkinson <Shaun.Wilkinson@redstor.com>
2025-05-22 09:28:11 +0100

    Added validation when retrieving mvp value. Added footer

--- options.js
+++ options.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const input = document.getElementById("mvpId");
-  
-	chrome.storage.sync.get({ mvpId: "missingConfig" }, (data) => {
-	  input.value = data.mvpId;
+
+	chrome.storage.sync.get({ mvpId }, (data) => {
+		if(data?.mvpId) {
+			const dataReturned = Object.keys(data.mvpId).length > 0
+			if(dataReturned > 0) {
+				input.value = data.mvpId;
+			}
+		}
 	});
-  
+
 	document.getElementById("save").addEventListener("click", () => {
-	  const mvpId = input.value.trim();
-	  if (mvpId) {
-		chrome.storage.sync.set({ mvpId }, () => {
-		  alert("MVP ID saved!");
-		});
-	  }
+		const mvpId = input.value.trim();
+		if (mvpId) {
+			chrome.storage.sync.set({ mvpId }, () => {
+				alert("MVP ID saved!");
+			});
+		}
 	});
-  });
-  
+});
